0affcb79d95e9ea6db415bd4f866d59ce63c99b4
greg-aires <hiepdt2909@gmail.com>
2023-01-10 12:34:51 +0700

    f

--- Lab3/MVC_simple/Core/Field.php
+++ Lab3/MVC_simple/Core/Field.php
@@ -1,20 +1,45 @@
 <?php
+namespace Core;
 class Field{
     public  const TYPE_TEXT ='test';
-    public const TYPE_WORD = 'password';
+    public const TYPE_PASSWORD = 'password';
     public const TYPE_NUMBER = 'number';
     public string $type;
-    public  string $atribute;
+    public string $atribute;
 
     public function __construct(string $atribute)
     {
         $this->type = self::TYPE_TEXT;
         $this->atribute = $atribute;
     }
-    public function __toString()
+    public function __toString(){
+        return sprintf(
+            '
+       <div class="form-group">
+        <label for="">%S</label>
+        <input type="%s" name="%s">
+       </div>
+       ',
+            $this->getLabel($this->atribute),
+            $this->type,
+            $this->atribute
+        );
+    }
+    public function PasswordField(){
+        $this -> type = self::TYPE_PASSWORD ;
+        return $this;
+    }
+    public function labels(): array{
+        return[
+            'firstname' => "First name",
+            'lastname' => "Last name",
+            'email' => "Your email",
+            'password' => "Password",
+            'Confirmpassword' => "Confirm password"
+        ];
+    }
+    public function getLabel($atribute)
     {
-       return printf('<div class="form-group">
-       <label>$s</label>
-       )
+        return $this -> labels()[$atribute] ?? $atribute ;
     }
 }
---
+++ Lab3/MVC_simple/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'boss/mvc_simple',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a331c848ec2ff2d91db50b4f685cfdf417daeec4',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'boss/mvc_simple' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a331c848ec2ff2d91db50b4f685cfdf417daeec4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
